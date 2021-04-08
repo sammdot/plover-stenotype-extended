5f6a1cfe6794b8eddcc3b2abef8016b05bb54dcb
Sammi De Guzman <sammi@sammdot.ca>
2021-04-08 10:38:55 -0700

    Initial commit

---
+++ extended_stenotype.py
@@ -0,0 +1,10 @@
+from plover.system.english_stenotype import *
+
+KEYS = (
+  '#', '^-', '+-',
+  'S-', 'T-', 'K-', 'P-', 'W-', 'H-', 'R-',
+  'A-', 'O-',
+  '*',
+  '-E', '-U',
+  '-F', '-R', '-P', '-B', '-L', '-G', '-T', '-S', '-D', '-Z',
+)
